2af1b3d702cd8ad520d6bd0c7b6548601990143b
Yuchen <caiy@rose-hulman.edu>
2024-05-05 19:53:31 -0400

    add plot, remove egoist
    Change opponent action, change variables for computing expected gain for self and opponent. Add plot for changes against playing round.

--- 1.py
+++ 1.py
@@ -3,29 +3,46 @@ import numpy as np
 import random
 
 #opponents behavior
-opponent_action=[1,1,1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1,0,1,0,1]
+#110000000000111111111101010101
+opponent_action=[1]*5+[0]*10+[1]*10+[0,1]*5
 
 #first number is whether self cooperate or cheat, second is opponent's
 
+#evolution of trust version
 m11=(1,1)
 m01=(2,-1)
 m10=(-1,2)
 m00=(0,0)
 
+#evolution of cooperation version
 # m11=(3,3)
 # m10=(0,5)
 # m01=(5,0)
 # m00=(1,1)
 
+#zero sum
+# m11=(0,0)
+# m01=(1,-1)
+# m10=(-1,1)
+# m00=(0,0)
 
-altruism=0.2
-egoist=1-altruism
+#deterrence
+# m11=(0,0)
+# m01=(10,-100)
+# m10=(-100,-100)
+# m00=(-100,-100)
+
+#in exponent, -1 means evaluate other as 1/e of self
+#experimental value is about -3 to -5
+altruism=-3
 
 
-decay=0.25#memory decay rate per round, to simulate the forgiveness for past
+#memory decay rate per round, to simulate the forgiveness for past
+#correspond to the active towards change
+#should be about 0.5 to 1
+decay=0.4
 
-#list to store all cooperate probabilities
-p_cooperates=[]
+#decay rate=1 and altruism=-infinity is tit-for-tat strategy
 
 #input a list and decay rate, return the probability for next decision is cooperate
 def next_probability(l,decay):
@@ -49,8 +66,10 @@ def next_probability(l,decay):
 #1: cooperate
 #0: cheat
 
-ha=[]
-hb=[]
+ha=[]#history of self decisions
+hb=[]#history of opponent decisions
+pa1s=[]#probability of self cooperate
+pb1s=[]#probability of estimated opponent cooperate
 i=0
 for b in opponent_action:
     i+=1
@@ -59,20 +78,22 @@ for b in opponent_action:
     
     pb1=next_probability(hb,decay)
     pb0=1-pb1
-    
+    pb1s.append(pb1)
     pa1=next_probability(ha,decay)
     pa0=1-pa1
     
     #expected gain by changing from cooperate to cheat in both situation
-    ea=abs((m10[0]-m11[0])*pb1+(m00[0]-m01[0])*pb0)+egoist
+    ea=(m01[0]-m11[0])*pb1+(m00[0]-m10[0])*pb0
     #expected gain of opponent
-    pb0=max(0.01,pb0)#still set a lower boundary to avoid divided by 0
-    et=abs((m10[1]-m11[1])*pb1+(m00[1]-m01[1])*pb0)+altruism+(pb1/pb0)
-    # print(ea,et)
+    pb0=max(0.01,pb0)#still set a lower boundary to avoid divided by 0, and more than 0.001 to avoid overflow exponent
+    cooperate_factor=pb1/pb0
+    #consider opponent gain by changing from cheat to cooperate, with altruism and probability variable
+    eb=(m11[1]-m01[1])*pb0+(m10[1]-m00[1])*pb1+altruism+cooperate_factor
+    print(ea,eb)
     #softmax to acquire probability
-    pa1=np.exp(et)/(np.exp(et)+np.exp(ea))
+    pa1=np.exp(eb)/(np.exp(eb)+np.exp(ea))
     pa0=1-pa1
-    p_cooperates.append(pa1)
+    pa1s.append(pa1)
     print(pa1)
     
     a=1
@@ -80,9 +101,21 @@ for b in opponent_action:
         a=0
     ha.append(a)
     
-print(p_cooperates)
 
 import matplotlib.pyplot as plt
-plt.plot(p_cooperates)
+plt.plot(pa1s,"k-")
+plt.plot(pb1s,"--")
+for i in range(len(ha)):
+    if ha[i]:
+        plt.plot(i,-0.05,"g.")
+    else:
+        plt.plot(i,-0.05,"r.")
+for i in range(len(hb)):
+    if hb[i]:
+        plt.plot(i-0.5,pa1s[i],"g.")
+    else:
+        plt.plot(i-0.5,pa1s[i],"r.")
+# plt.plot()
+plt.ylim(-0.1,1.1)
 plt.show()
     
